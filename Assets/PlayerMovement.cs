f2f814ca3003d8b12624e0fcfbd5090f23dab70a
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-09-29 02:26:34 +0530

    Attach Camera

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,8 +8,19 @@ public class PlayerMovement : MonoBehaviour {
 		
 	// }
 	public float forwardForce = 2000f;
+	public float sideWaysForce = 500f;
 	// Update is called once per frame
 	void FixedUpdate () {
 		rb.AddForce(0,0,forwardForce*Time.deltaTime);
+
+		if(Input.GetKey("d"))
+		{
+			rb.AddForce(sideWaysForce*Time.deltaTime,0,0);
+		}
+		if(Input.GetKey("a"))
+		{
+			rb.AddForce(-1*sideWaysForce*Time.deltaTime,0,0);
+		}
+
 	}
 }
