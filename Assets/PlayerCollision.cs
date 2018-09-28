bc9118dcc94b5a0379120751e7065e00150c6d36
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-09-29 04:09:12 +0530

    Add health system

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	// Use this for initialization
+	// void Start () {
+		
+	// }
+	
+	// Update is called once per frame
+	// void Update () {
+		
+	// }
+	public Rigidbody rb;
+	public PlayerMovement movement;
+	public float health = 100f;
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		if(collisionInfo.collider.tag=="Obstacle")
+		{
+			health = health - 34;
+			Debug.Log(health);
+			Collider.Destroy(collisionInfo.collider);
+			if(health>0)
+			{
+
+			}
+			else
+			{
+				movement.enabled = false;
+			}
+		}
+	}
+}
