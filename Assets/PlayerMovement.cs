36ca2b98cc549dd4ab63ea9600bafeb59286119f
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-09-29 04:57:05 +0530

    Movement integrated

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,22 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public Rigidbody rb;
-	public float forwardForce = 2000f;
-	public float sideWaysForce = 500f;
+	public Rigidbody spaceShipBody;
+	public float forwardSpeed = 1.0f;
 
-	void FixedUpdate () {
-		rb.AddForce(0,0,forwardForce*Time.deltaTime);
-
-		if(Input.GetKey("d"))
-		{
-			rb.AddForce(sideWaysForce*Time.deltaTime,0,0);
-		}
-		if(Input.GetKey("a"))
-		{
-			rb.AddForce(-1*sideWaysForce*Time.deltaTime,0,0);
-		}
-
+	void Start () {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		spaceShipBody = GetComponent<Rigidbody>();
 	}
+
+	public float g=9.8f;
+
+    void FixedUpdate() {
+        var gravity = new Vector3(Input.acceleration.x * 2.5f, 0, forwardSpeed)*g;
+
+        spaceShipBody.AddForce (gravity);
+    }
 }
---
+++ Assets/Script/SpaceshipMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+	public Rigidbody spaceShipBody;
+	public float forwardSpeed = 1.0f;
+
+	void Start () {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		spaceShipBody = GetComponent<Rigidbody>();
+	}
+
+	public float g=9.8f;
+
+    void FixedUpdate() {
+        var gravity = new Vector3(Input.acceleration.x * 2.5f, 0, forwardSpeed)*g;
+
+        spaceShipBody.AddForce (gravity);
+    }
+}
