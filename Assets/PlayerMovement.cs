2fc1157b05f8ae19cafaadbb01a414f02d87e620
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-09-29 06:58:28 +0530

    Rotation Completed

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody spaceShipBody;
-	public float forwardSpeed = 1.0f;
+	public float forwardSpeed = 20.0f;
 
 	void Start () {
 		Screen.orientation = ScreenOrientation.LandscapeLeft;
@@ -15,7 +15,15 @@ public class PlayerMovement : MonoBehaviour {
 	public float g=9.8f;
 
     void FixedUpdate() {
-        var gravity = new Vector3(Input.acceleration.x * 2.5f, 0, forwardSpeed)*g;
+        var gravity = new Vector3(Input.acceleration.x * 15.0f, 0, 0)*g;
+		var curr_pos = GameObject.Find("Player").transform.position;
+		var curr_x = curr_pos.x;
+		var curr_y = curr_pos.y;
+		
+		var angleBoardRotX = Mathf.Atan(curr_y/curr_x)*100;
+		Debug.Log(angleBoardRotX);
+
+		transform.Rotate(new Vector3(0,0,angleBoardRotX) * Time.deltaTime);
 
         spaceShipBody.AddForce (gravity);
     }
